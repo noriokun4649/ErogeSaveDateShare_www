7a0d966b547ba5d80e3b5998283dfa81d104a307
noriokun 4649 <noriokun 4649>
2017-10-30 05:11:19 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfApp1/AddWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public AddWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Title = "ファイルを開く";
+            dialog.Filter = "全てのファイル(*.*)|SAVEファイル(*.sav)";
+            if (dialog.ShowDialog() == true)
+            {
+                this.file_box.Text = dialog.FileName;
+            }
+            else
+            {
+                this.file_box.Text = "キャンセルされました";
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "○○するフォルダーを選択してください。あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
+            if (folderBrowserDialog.ShowDialog(this) == DialogResult.Cancel)
+            {
+                // [キャンセル] をクリックされた場合は終了
+                return;
+            }
+            // フォルダーパスを取得
+            string fileName = folderBrowserDialog.SelectedPath;
+            // ダイアログのインスタンスを破棄する
+            folderBrowserDialog.Dispose();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            checkbox_drop.IsChecked = Properties.Settings.Default.dropbox_snyc;
+            checkbox_pc.IsChecked = Properties.Settings.Default.pc_snyc;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result= MessageBox.Show("DropBoxと連携を行います。\n\n連携をしますか？",
+            "DropBox連携",
+            MessageBoxButton.YesNo,
+            MessageBoxImage.Information);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Properties.Settings.Default.dropbox_conectings = true;
+            }
+            else if (result == MessageBoxResult.No)
+            {
+
+            }
+        }
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox c = (CheckBox)sender;
+
+            Properties.Settings.Default.pc_snyc = (bool)c.IsChecked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {
+            CheckBox c = (CheckBox)sender;
+
+            Properties.Settings.Default.dropbox_snyc = (bool)c.IsChecked;
+            Properties.Settings.Default.Save();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var win = new AddWindow();
+            win.Owner = this;
+            win.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

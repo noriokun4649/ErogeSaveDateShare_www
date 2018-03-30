2aa7e0010ace88a109f3f3b55953f5f44ba9fedd
noriokun4649 <noriokun4649@gmail.com>
2018-03-30 20:09:25 +0900

    コードとレイアウトをじっそー

--- WpfApp1/AppInfoPage.xaml.cs
+++ WpfApp1/AppInfoPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,7 +23,70 @@ namespace WpfApp1
     {
         public AppInfoPage()
         {
-            InitializeComponent();
+            InitializeComponent(); System.Reflection.Assembly assembly = Assembly.GetExecutingAssembly();
+            System.Reflection.AssemblyName asmName = assembly.GetName();
+            System.Version versions = asmName.Version;
+            version.Text = "Version V." + versions.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)//blog
+        {
+
+            try
+            {
+                System.Diagnostics.Process.Start("https://noriokun4649.blog.fc2.com/");
+            }
+            catch { }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)//twitter
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://twitter.com/noriokun_blog");
+            }
+            catch { }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)//Mail
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)//Git
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://github.com/noriokun4649/ErogeSaveDateShare_www");
+            }
+            catch { }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)//Drop
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://github.com/dropbox/dropbox-sdk-dotnet");
+            }
+            catch { }
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)//meta
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://github.com/ButchersBoy/MaterialDesignInXamlToolkit");
+            }
+            catch { }
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)//win
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://github.com/aybe/Windows-API-Code-Pack-1.1");
+            }
+            catch { }
         }
     }
 }
